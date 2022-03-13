72eb12f40ab45706418d2897a9be5bf936714f22
simone00-cyber <101263128+simone00-cyber@users.noreply.github.com>
2022-03-13 17:23:34 +0100

    HomeScene

--- public/game.js
+++ public/game.js
@@ -1,6 +1,5 @@
 import {Laser,LaserGroup} from './entity/laser.js';
 import {Alien,AlienGroup,AlienLaserGroup} from './entity/alien.js';
-
 var score = 0;
 var scoreText;
 var ammo = 3;
@@ -16,6 +15,17 @@ var classification;
 var pointText;
 var point = [];
 var button;
+var image;
+var image1;
+var image2;
+var image3;
+var image4;
+var image5;
+var formCreateRoom;
+var formJoinRoom;
+var activeDescription;
+var lobby;
+var start=false;
 function removeAlien(alien,laser){
 	var explosion = this.sound.add('explosion');	
 	alien.disableBody(true,true);
@@ -44,6 +54,7 @@ function shipHit(ship,laser){
 	}
 	
 }
+
 
 
 class GameScene extends Phaser.Scene
@@ -75,6 +86,7 @@ class GameScene extends Phaser.Scene
 	}
 
 	create() {
+		//lobby.stop();
 		socket = io();
 		
 		
@@ -182,17 +194,40 @@ class GameScene extends Phaser.Scene
 		});
 
 		this.input.keyboard.on('keydown_R', this.reloadAmmo, this);
+		
+		socket.on("message", function(data){
+			var i = 1;
+			var Leaderboard = "";
+           /*
+			var ordered = Object.keys(data).sort().reduce(
+				(obj, key) => { 
+				  obj[key] = data[key]; 
+				  return obj;
+				}, 
+				{}
+			);*/
+			var ordered = {};
+			Object.entries(data)
+			.sort((a, b) => a[1] - b[1])
+			.forEach(([key, val]) => {
+				ordered[key] = val;
+			});
+			for(var key in ordered) {
+				if(ordered.hasOwnProperty(key)) {
+					var propValue = ordered[key];
+					Leaderboard = Leaderboard+i+'.'+key+'\t'+propValue+"\n";
+					i++;
+				}
+			};
+			pointText.setText(Leaderboard);
+		});
 	}
 	fireBullet() {
 		this.laserGroup.fireBullet(this.ship.x, this.ship.y - 20);
 	}
 	update(time) {
+		
 		socket.emit(socket.id,score);
-		socket.on("message", function(data){
-				point = JSON.stringify(data);
-				var jsonPretty = JSON.stringify(JSON.parse(point),null,2); 
-				pointText.setText('1.       '+jsonPretty);
-		 }); 
 		if(timedEvent.getProgress().toString().substr(0, 4)<0.60)
 		{
 			timeText.setText('Time: ' + timedEvent.getProgress().toString().substr(0, 4));
@@ -236,6 +271,140 @@ class GameScene extends Phaser.Scene
 		
 	}
 }
+class HomeScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('HomeScene');
+    }
+ 
+	preload() {
+		this.load.image('background', './assets/background.jpg');
+		this.load.audio('lobby', [ './audio/lobby.ogg', './audio/lobby.mp3' ]);
+		this.load.html('login', './room.html');
+		this.load.html('joinRoom', './joinRoom.html');
+		this.load.html('description', './description.html');
+		this.load.image('input2', './assets/input2.png');
+		this.load.image('buttom', './assets/buttom.png');
+		
+	}
+    create ()
+    {
+		
+        lobby = this.sound.add('lobby');
+		lobby.play();	
+		this.add.image(550,350,'background');
+		this.add.image(200,150,'buttom');
+		this.add.image(450,150,'buttom');
+		image1=this.add.image(350,250,'input2');
+		image2=this.add.image(350,350,'input2');
+		image3=this.add.image(350,450,'input2');
+		image4=this.add.image(350,570,'buttom');
+		image5=this.add.image(350,470,'buttom');
+        var createRoom = this.add.dom(400, 600).createFromCache('login');
+		createRoom.addListener('click');
+
+	    var joinRoom = this.add.dom(400, 600).createFromCache('joinRoom');
+		joinRoom.addListener('click');
+
+		//var description = this.add.dom(300, 400).createFromCache('description');
+		
+		
+		
+		image1.visible=true;
+		image2.visible=true;
+		image3.visible=true;
+		image4.visible=true;
+		image5.visible=false;
+		
+        createRoom.on('click', function (event) {
+
+			if (event.target.name === 'createRoom')
+			{
+				var roomName = this.getChildByName('roomName');
+				var nickname = this.getChildByName('nickname');
+				var players = this.getChildByName('players');
+				if(roomName.value != '' && roomValue!=0)
+				{
+					this.scene.start('GameScene');
+				}
+			}
+			
+			if (event.target.name === 'viewRoom')
+			{
+				
+				formCreateRoom = this.getChildByName('createRoom');
+				
+				if(formCreateRoom.style.display === 'none')
+				{
+					formJoinRoom.style.display='none';
+					formCreateRoom.style.display='block';
+					image1.visible=true;
+					image2.visible=true;
+					image3.visible=true;
+					image4.visible=true;
+					image5.visible=false;
+					
+					
+				}
+				else
+				{
+					formCreateRoom.style.display='none';
+					image1.visible=false;
+					image2.visible=false;
+					image3.visible=false;
+					image4.visible=false;
+				}
+			}
+	
+		});
+		
+		joinRoom.on('click', function (event) {
+
+			
+			if (event.target.name === 'viewJoin')
+			{
+				
+				formJoinRoom = this.getChildByName('joinRoom');
+				if(formJoinRoom.style.display === 'none')
+				{
+					formJoinRoom.style.display='block';
+					
+					
+					image1.visible=true;
+					image2.visible=true;
+					image3.visible=false;
+					image4.visible=false;
+					formCreateRoom.style.display='none';
+					image5.visible=true;
+					
+				}
+				else
+				{
+					formJoinRoom.style.display='none';
+					image1.visible=false;
+					image2.visible=false;
+					formCreateRoom.style.display='none';
+					image5.visible=false;
+				}
+			}
+	
+		});
+		this.tweens.add({
+			targets: element,
+			y: 300,
+			duration: 3000,
+			ease: 'Power3'
+		});
+    }
+	update()
+	{
+		if(start === true)
+		{
+			this.scene.start('GameScene');
+		}
+	}
+}
 class GameOver extends Phaser.Scene {
 
     constructor ()
@@ -267,6 +436,9 @@ const config = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
+	dom: {
+        createContainer: true
+    },
 	physics: {
 		default: 'arcade',
 		arcade: {
