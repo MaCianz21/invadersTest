2ba020e758d680bd4bc41c3e8beeb3f07235ee8f
simone00-cyber <101263128+simone00-cyber@users.noreply.github.com>
2022-03-16 16:28:07 +0100

    multiplayer

--- server.js
+++ server.js
@@ -4,24 +4,59 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-var numPlayer=0;
+var player={};
 app.use(express.static(__dirname + '/public'));
 var point={};
 var id;
+var roomArray={};
+var numRoom=0;
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', (socket) => {
-  numPlayer++;
+  
   console.log('user '+socket.id+' connected');
 
+  socket.on('createRoom', function(room) {
+    
+    var roomName = room.name;
+    roomArray[roomName] = {};
+    player[roomName]=room.numberPlayer -1;
+    socket.join(roomArray);
+    console.log('Room '+roomName+' created  '+numRoom );
+    console.log(player[roomName]);
+  });
+ 
+    
+    
+   
+  socket.on('joinRoom', function(room) {
+    
+    var roomName = room.name;
+
+    socket.join(room.name);
+    roomArray[roomName][room.nickname] = 0;
+    player[roomName] = player[roomName]-1;
+    console.log('Player '+room.nickname+' has joined in the  '+room.name );
+    console.log(player[roomName]);
+    io.to(socket.id).emit('players',player[roomName]);
+     
+   
+  });
   socket.on(socket.id, (msg) => {
-    point[socket.id] = msg;
-    //console.log(point);
-    io.emit('message',point);
+
+    var nick = msg.nickname;
+    var roomName = msg.nameRoom;
+
+    roomArray[roomName][nick] =  msg.score;
+    
+    var tmp = roomArray[roomName];
+    io.to(socket.id).emit('message',tmp);
+    
   });
   socket.on('disconnect', () => {
     delete point[socket.id];
+    
     console.log('user '+socket.id+' disconnected');
   });
 });
